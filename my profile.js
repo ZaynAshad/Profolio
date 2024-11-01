9bd8b0e3a16f6f4086143ba3bfd7273c884c0114
ZaynAshad <114851535+ZaynAshad@users.noreply.github.com>
2024-11-01 09:51:55 +0000

    Add files via upload

---
+++ my profile.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById("contactForm").addEventListener("submit", function(event){
+        event.preventDefault(); // Prevents the form from submitting traditionally
+        alert("Thank you for your message !");
+    });
+</script>
